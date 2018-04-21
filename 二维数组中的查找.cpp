cd9306d19d82c13eb185454c83519f5dbb9d61ed
soldierbro <xiaocb1992@163.com>
2018-04-21 20:34:02 +0800

    Create 二维数组中的查找.cpp

---
+++ 二维数组中的查找.cpp
@@ -0,0 +1,20 @@
+```cpp
+class Solution {
+public:
+    bool Find(int target, vector<vector<int> > array) {
+        if(array.size()==0||array[0].size()==0)
+            return false;
+        int m=0;
+        int n=array[0].size()-1;
+        while(m<array.size()&&n>=0){
+            if(array[m][n]==target)
+                return true;
+            else if(target<array[m][n])
+                n--;
+            else
+                m++;
+        }
+        return false;
+    }
+};
+```
